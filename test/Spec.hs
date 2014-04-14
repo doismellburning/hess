4ac67e1160af00242dac370f5bbd2c422eeec097
Kristian Glass <git@doismellburning.co.uk>
2014-04-15 00:21:25 +0100

    Slightly tidier test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 import Test.Hspec
 import Hess.Core
 
+import Data.Maybe
+
 pawnlessFEN = "rnbqkbnr/8/8/8/8/8/8/RNBQKBNR w KQkq - 0 1"
 simpleRookFEN = "k7/8/8/8/8/8/8/3R4 w - - 0 1"
 simpleBishopFEN = "k7/8/8/8/8/8/8/3B4 w - - 0 1"
@@ -13,8 +15,8 @@ main = hspec $ do
             let
                 fens = [startingFEN, pawnlessFEN, simpleRookFEN, simpleBishopFEN, checkFEN]
                 states = map fromFEN fens :: [Maybe GameState]
-                readwritten = map (fmap toFEN) states
-                in readwritten `shouldBe` (map Just fens)
+                readwritten = map (fmap toFEN) states :: [Maybe String]
+                in (map (fromMaybe ":(") readwritten) `shouldBe` fens
 
         it "starts in the right state" $ do
             (toFEN newGame) `shouldBe` startingFEN
